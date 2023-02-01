878ffa3ac82ea8ed7ce258d952eefa0666da53e8
Artem Lebedev <lebedevam91@mail.ru>
2023-02-01 21:39:56 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsMap = new Map();
+
+  for (let config of bracketsConfig) {
+    bracketsMap.set(config[1], config[0]);
+  }
+
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (bracketsMap.has(char)) {
+      const top = stack[stack.length - 1];
+
+      if (top === bracketsMap.get(char)) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      for (let config of bracketsConfig) {
+        if (char === config[0]) {
+          stack.push(char);
+          break;
+        }
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
